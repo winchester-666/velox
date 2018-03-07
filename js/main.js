27a9cea40048e2e92949ab746827fe087eec2c24
LENOVO <info@exsy.com.ua>
2018-03-07 23:37:23 +0200

    second

--- js/main.js
+++ js/main.js
@@ -8,5 +8,47 @@ jQuery(document).ready(function($) {
 		centerPadding: '60px',
 		dots: true,
 		infinite: true,
+		responsive: [
+		{
+			breakpoint: 768,
+			settings: {
+				centerPadding: '40px',
+				slidesToShow: 3
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				centerPadding: '40px',
+				slidesToShow: 1
+			}
+		}
+		]
+	});
+
+
+	$('#scroll_top').click(function() {
+		$('html, body').animate({scrollTop: 0},800);
+		return false;
+	});
+	$(window).scroll(function(){
+		$('header').toggleClass('header_background', $(this).scrollTop() > 300);
+	});
+
+	$('#menu-toggle').click(function(){
+		$(this).toggleClass('open');
+		$('.header_menu').toggleClass('open_menu');
+	});
+
+	$(".portfolio_list_cat li").click(function(event) {
+		$(".portfolio_list_cat li").removeClass('active');
+		$(this).addClass('active');
+	});
+
+	$('a[href^="#scroll_"]').click(function(){
+		var el = $(this).attr('href');
+
+		$('html, body').animate({ scrollTop: $(el).offset().top}, 1000);
+		return false;
 	});
 });
